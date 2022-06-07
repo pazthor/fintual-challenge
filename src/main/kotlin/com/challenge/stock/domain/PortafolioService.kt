059bb310a393825d964d8b5a67aa115e30156f77
Julio <hi.pazthor@gmail.com>
2022-06-06 22:43:40 -0500

    add get profit in  a specific time

--- src/main/kotlin/com/challenge/stock/domain/PortafolioService.kt
+++ src/main/kotlin/com/challenge/stock/domain/PortafolioService.kt
@@ -1,12 +1,17 @@
 package com.challenge.stock.domain
 
 import java.time.LocalDate
-
 class PortafolioService(val portafolio: Portafolio) {
 
-    fun getProfit(start: LocalDate, end: LocalDate): List<Stock>{
+    fun getProfit(start: LocalDate, end: LocalDate): List<Stock> {
+        val stocksList = this.portafolio.stocksList
 
+        val result = stocksList.filter {
+                stock -> stock.created_at.isAfter(start) && stock.created_at.isBefore(end)
+        }
 
-        return portafolio.stocksList
+        return result
     }
 }
+
+
--- src/test/kotlin/domain/PortafolioServiceTest.kt
+++ src/test/kotlin/domain/PortafolioServiceTest.kt
@@ -10,18 +10,25 @@ import kotlin.test.assertEquals
 class PortafolioServiceTest {
 
     @Test
-    fun `it should get 1 stock from portafolio successfully`() {
+    fun `it should get 2 stock from portafolio successfully`() {
         val stocksList = mutableListOf<Stock>()
         val localDateStart =  LocalDate.of(2021,10,1)
         val localDateEnd =  LocalDate.of(2021,12,1)
-        val stock1 = Stock(100.1,localDateStart )
+        val createStock1 =  LocalDate.of(2021,10,2)
+        val createStock2 =  LocalDate.of(2021,11,30)
+        val stock1 = Stock(100.1,createStock1 )
+        val stock2 = Stock(200.1,createStock2 )
+
         stocksList.add(stock1)
+        stocksList.add(stock2)
         val portafolio = Portafolio(stocksList)
 
         val service = PortafolioService(portafolio)
         val profitSize = service.getProfit(localDateStart, localDateEnd).size
-        val expectedProfit = 1
+        val expectedProfit = 2
 
         assertEquals(expectedProfit, profitSize)
     }
+
+
 }
